a791a4413495e14a5232c43d31b4f460c438e423
huggywuggyMaster <127624065+huggywuggyMaster@users.noreply.github.com>
2024-05-05 18:18:58 +0200

    Update mainhack.py

--- mainhack.py
+++ mainhack.py
@@ -1,35 +1,12 @@
+import time
 
-import os
-import shutil
-import sys
-import subprocess
+def codething(): 
+    print("you noob :)")
 
-def add_to_startup(file_path):
-    if sys.platform.startswith('win'):
-        startup_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
-    elif sys.platform.startswith('darwin'):
-        startup_folder = os.path.join(os.getenv('HOME'), 'Library', 'LaunchAgents')
-    else:
-        return
+    time.sleep(10)
+    haha()
     
-    try:
-        shutil.copy(file_path, startup_folder)
-    except Exception as e:
-        pass
+def haha():
+    codething()
 
-def main():
-    file_path = os.path.abspath(__file__)
-    
-    if sys.platform.startswith('win'):
-        startup_folder = os.path.join(os.getenv('APPDATA'), 'Microsoft', 'Windows', 'Start Menu', 'Programs', 'Startup')
-        if not os.path.isfile(os.path.join(startup_folder, os.path.basename(file_path))):
-            add_to_startup(file_path)
-    elif sys.platform.startswith('darwin'):
-        startup_folder = os.path.join(os.getenv('HOME'), 'Library', 'LaunchAgents')
-        if not os.path.isfile(os.path.join(startup_folder, os.path.basename(file_path))):
-            add_to_startup(file_path)
-        
-
-if __name__ == "__main__":
-    main()
-    
+codething()    
